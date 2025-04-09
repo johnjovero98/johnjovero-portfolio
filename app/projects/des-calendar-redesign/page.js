e58face42361466bb29d781e080c37514f9b2e6f
johnjovero98 <john.jovero@outlook.com>
2025-04-09 00:51:27 -0600

    finish katppy

--- app/projects/des-calendar-redesign/page.js
+++ app/projects/des-calendar-redesign/page.js
@@ -29,7 +29,7 @@ export default async function DESCalProjectPage() {
 
             <section className="container mx-auto px-1">
 
-                <h2 className="text-h2 font-bold text-balance mt-2 mb-2 text-blue-300 text-center">DES Calendar UI Redesign</h2>
+                <h2 className="text-h4 lg:text-h2 font-bold text-balance mt-2 mb-2 text-blue-300 text-center">DES Calendar UI Redesign</h2>
 
 
                 <div className="project-content">
@@ -47,42 +47,44 @@ export default async function DESCalProjectPage() {
                     <div className="project-details-section">
                         {/* Task */}
                         <ProjectDetailSection title={"Task"}>
-                            <p className="text-balance mb-1">This is the first feature I updated that got into production. By the time I successfully completed this project, I felt confident and sure of myself that I had chosen the right path to become a web developer!</p>
-
-                            <p className="text-balance mb-1">This UI redesign project is an open-source contribution for the Dev Edmonton Society website. Although the past calendar design is functional, my goal for this project is to further improve the calendar UI component’s aesthetic and usability.</p>
+                            <p>This is the first feature I updated that got into production. By the time I successfully completed this project, I felt confident and sure of myself that I had chosen the right path to become a web developer!</p>
+                            <p>This UI redesign project is an open-source contribution for the Dev Edmonton Society website. Although the past calendar design is functional, my goal for this project is to further improve the calendar UI component’s aesthetic and usability.</p>
                         </ProjectDetailSection>
 
 
                         {/* Features */}
                         <ProjectDetailSection title={"Features"}>
-                            <h4 className="font-bold text-h6  leading-[3rem]" >Major Changes:</h4>
-                            <ul className="list-disc pl-1">
+                            <h4>Major Changes:</h4>
+                            <ul>
                                 <li>Fix &quot;All day&quot; view</li>
                                 <li>Fix Overall Layout</li>
                                 <li>Follow design patterns for calendar UI components</li>
                             </ul>
 
-                            <p className="font-bold text-h6 leading-[3rem]" >UI Before:</p>
+
+                            <h4>UI Before:</h4>
                             <Image
                                 src={"/projects-img/calendar-ui-before.png"}
-                                alt="John Jovero"
+                                alt="Calendar UI before the revisions"
                                 width={600}
                                 height={600}
                                 className="mb-1 border border-gray-200 rounded-lg shadow-md"
                             />
 
-                            <p className="font-bold text-h6 leading-[3rem]" >UI After:</p>
+
+                            <h4>UI After (Month View):</h4>
                             <Image
                                 src={"/projects-img/calendar-month-view-demo.gif"}
-                                alt="John Jovero"
+                                alt="Calendar UI after the revisions month view"
                                 width={600}
                                 height={600}
                                 className="mb-1 border border-gray-200 rounded-lg shadow-md"
                             />
 
+                            <h4>UI After (Week View):</h4>
                             <Image
                                 src={"/projects-img/calendar-week-view-demo.gif"}
-                                alt="John Jovero"
+                                alt="Calendar UI after the revisions week view"
                                 width={600}
                                 height={600}
                                 className="mb-1 border border-gray-200 rounded-lg shadow-md"
@@ -92,7 +94,7 @@ export default async function DESCalProjectPage() {
 
                         {/* Results */}
                         <ProjectDetailSection title={'Results'}>
-                            <p className="text-balance">My revisions got successfully deployed in the production– increasing the event&apos;s page visitation rate. I also received some positive feedback on the design and usability.</p>
+                            <p>My revisions got successfully deployed in the production– increasing the event&apos;s page visitation rate. I also received some positive feedback on the design and usability.</p>
                         </ProjectDetailSection>
                     </div>
                 </div>
--- app/projects/katppy-ecommerce-website/page.js
+++ app/projects/katppy-ecommerce-website/page.js
@@ -27,7 +27,7 @@ export default async function DESCalProjectPage() {
             </div>
 
             <section className="container mx-auto px-1">
-                <h2 className="text-h2 font-bold text-balance mt-2 mb-2 text-blue-300 text-center">Katppy E-Commerce Website (Ongoing)</h2>
+                <h2 className="text-h4 lg:text-h2 font-bold text-balance mt-2 mb-2 text-blue-300 text-center">Katppy E-Commerce Website (Ongoing)</h2>
 
                 <div className="project-content">
 
@@ -49,41 +49,114 @@ export default async function DESCalProjectPage() {
                         </ProjectDetailSection>
 
                         <ProjectDetailSection title={"Goals"}>
-                            <p className="mb-1" >This project is assigned for my DMIT Capstone Project at NAIT along with other two members.</p>
-                            <p className="mb-1" ><strong>Role:</strong> Project Leader, Developer </p>
-                            <p className="mb-1" >The goal of the project was to create a custom, visually appealing, and user-friendly WooCommerce store tailored to Katppy’s niche audience such as DIY crafters and collectors.</p>
-                            <p className="mb-1">Additionally, our client wants to decrease the service cost of running an online shop, so she decides to switch from Etsy to WooCommerce.</p>
 
-                            <strong>Personal Goal:</strong> 
-                            <p className="mb-1">My personal goal for this project is to build a thousand-dollar looking e-commerce website to showcase in my portfolio.</p>
+                            <p>I was assigned, along with other 2 members, to build this website as a capstone project.</p>
+                            <p><strong>Role:</strong> Project Leader, Developer </p>
+                            <p>The goal of the project was to create a custom, visually appealing, and user-friendly WooCommerce store tailored to Katppy’s niche audience such as DIY crafters and collectors.</p>
+                            <p>Additionally, our client wants to decrease the service cost of running an online shop, so she decides to switch from Etsy to WooCommerce.</p>
+
+                            <strong>Personal Goal:</strong>
+                            <p>My personal goal for this project is to build a thousand-dollar looking e-commerce website to showcase in my portfolio.</p>
                         </ProjectDetailSection>
 
 
                         <ProjectDetailSection title={"Development"}>
+                            <p>My role as a lead developer is to ensure the technical requirement are met, while maintaining best practices. Such actions include developing a custom WordPress theme and WooCommerce features,  integrating third-party services (email, payments, marketing), and making sure all deliverables are finishined in a timely manner.
+                            </p>
+
+                            <p>I also collaborated with my design team and client to align decisions with Katppy’s brading and aesthetic.</p>
+
                             <h4>Custom Theme Development</h4>
+                            <ul>
+                                <li>Built the site using the Underscores (_s) WordPress starter theme for flexibility and performance</li>
+                                <li>Developed custom plugins to showcase products and reviews on the landing page</li>
+                            </ul>
 
                             <h4>TailwindCSS Integration</h4>
+                            <ul>
+                                <li>Integrated TailwindCSS into the WordPress theme to rapidly build a clean and modern-looking UI.</li>
+                                <li>Combined Tailwind with WooCommerce templates for a cohesive and fully responsive shopping experience.</li>
+                            </ul>
 
                             <h4>WooCommerce Customization</h4>
+                            <ul>
+                                <li>Integrated Swiper.js to create a dynamic product image gallery with a zoom modal on click.</li>
+                                <li>Implemented AJAX filter and search bar to improve online shopping experience</li>
+                            </ul>
 
                             <h4>Mailchimp Integration</h4>
+                            <ul>
+                                <li>Integrated Mailchimp for email marketing and customer retention.</li>
+                                <li>Prepared for future campaigns like welcome emails, new arrivals, and seasonal promos to keep the community engaged.</li>
+                            </ul>
 
-                            <h4>Stripe Integration (In Progress)</h4>
+                            <h4>Stripe Integration</h4>
+                            <ul>
+                                <li>Set up Stripe as the future payment gateway for seamless and secure checkout</li>
+                                <li>Enabled express checkout like Apple Pay, and Google Pay options to improve the payment process speed</li>
+                            </ul>
 
                             <h4>Gmail SMTP Integration</h4>
-
-
+                            <ul>
+                                <li>Configured Gmail SMTP for reliable and secure transactional email delivery.</li>
+                                <li>Ensures that contact form messages land in the inbox (not spam), supporting seamless customer communication.</li>
+                            </ul>
                         </ProjectDetailSection>
 
 
 
                         <ProjectDetailSection title={"Preview"}>
-                            <p>The shop is not live in public since we're still testing and migrating some products, so here is a preview of it</p>
+
+                            <p>The shop is not live in public since we&apos;re still testing and migrating some products, so here is a demo video of it.</p>
+
+                            <h4>Shop page preview:</h4>
+                            <div className="mb-1 border border-solid border-gray-100 shadow-lg rounded-lg w-fit">
+                                <Image
+                                    src={"/projects-img/katppy-shop-page.png"}
+                                    alt="katppy shop page preview"
+                                    width={600}
+                                    height={600}
+                                    className="w-full max-w-[600px] rounded-lg"
+                                />
+                            </div>
+
+
+
+
+                            <h4>Product detail page preview:</h4>
+                            <div className="mb-1 border border-solid border-gray-100 shadow-lg rounded-lg w-fit">
+
+                                <Image
+                                    src={"/projects-img/katppy-product-detail-page.png"}
+                                    alt="katppy shop page preview"
+                                    width={600}
+                                    height={600}
+                                    className="w-full max-w-[600px] rounded-lg"
+                                />
+
+                            </div>
+
+
+
+                            <h4>Order Confirmation Email via Gmail SMTP:</h4>
+                            <div className="mb-1 border border-solid border-gray-100 shadow-lg rounded-lg w-fit">
+                                <Image
+                                    src={"/projects-img/order-email-sample.png"}
+                                    alt="Email sample for order confirmation"
+                                    width={600}
+                                    height={600}
+                                    className="w-full max-w-[600px] rounded-lg"
+
+                                />
+                            </div>
+
+
+
                         </ProjectDetailSection>
 
                         <ProjectDetailSection title={"Results"}>
                             Successfully migrating the content into production.
-                            Established a solid eCommerce foundation for the business to grow and scale product offerings.
+                            Established a solid e-commerce foundation for the business to grow and scale product offerings.
                         </ProjectDetailSection>
                     </div>
                 </div>
